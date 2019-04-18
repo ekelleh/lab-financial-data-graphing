a43843cb5be7b723f1150c4fb82e5e58c6d1548d
Helleke Heikkine <helleke.heikkinen@gmail.com>
2019-04-18 12:41:14 +0200

    iteration 2 done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,8 +1,30 @@
+const printCharts = (labels, prices) => {
+    const ctx = document.getElementById("myChart").getContext("2d");
+    const chart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    backgroundColor: "rgba(255, 99, 132, 0.2)",
+                    label: "Bitcoin Price",
+                    data: prices,
+                },
+            ],
+        },
+    });
+};
+
 const getBitcoinData = bitcoinData => {
     axios
         .get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
         .then(response => {
-            console.log(response.data);
+            const { data } = response;
+            console.log(data);
+            const labels = Object.keys(data.bpi);
+            const prices = Object.values(data.bpi);
+
+            printCharts(labels, prices);
         })
         .catch(err => {
             console.error(err);
